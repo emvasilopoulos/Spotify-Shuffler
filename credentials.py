654e26755a80c1d5773b841eac633dbffd86a633
ManosVasilopoulos <54832917+ManosVasilopoulos@users.noreply.github.com>
2019-10-17 22:52:41 +0300

    Delete credentials.py

--- credentials.py
+++
@@ -1,6 +0,0 @@
-# Register your app in developer.spotify.com/dashboard/applications
-SPOTIPY_CLIENT_ID='Your Spotify Client ID'
-SPOTIPY_CLIENT_SECRET='Your Spotify Client Secret'
-
-# You can choose another URI.
-SPOTIPY_REDIRECT_URI='https://google.com'
